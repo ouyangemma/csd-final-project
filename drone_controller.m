47f0610e571a370592314e2b732a656e9a5d2ba9
Isaac-Urquidi <iurquid1@jh.edu>
2025-11-10 10:33:59 -0500

    Updates on Function and Linear Simulink

--- drone_controller.m
+++ drone_controller.m
@@ -1,12 +1,11 @@
 
-function K = drone_controller()
 
 % LQR
 Q = eye(12); % Cost function weighing states
-R = ones(4,1); % Cost function weighing inputs
+R = eye([4, 4]); % Cost function weighing inputs
 N = 0; % Optional cross term matrix
 
 [K, S, P] = lqr(p.A_lin, p.B_lin, Q, R, N);
 
-end
+
 
