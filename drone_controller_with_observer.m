f6ae1e4ed88f96352a8c68f6fa2ec0894da32ce6
Isaac-Urquidi <iurquid1@jh.edu>
2025-11-17 12:10:47 -0500

    Observer Finished
    Finished Observer on linear model

--- drone_controller_with_observer.m
+++ drone_controller_with_observer.m
@@ -1,9 +1,28 @@
 
 
 % LQR
+
 Q = eye(12); % Cost function weighing states
 R = eye([4, 4]); % Cost function weighing inputs
 N = 0; % Optional cross term matrix
 
 [K, S, P] = lqr(p.A_lin, p.B_lin, Q, R, N);
 
+%%%
+
+% LQE
+%%
+
+C = zeros(6,12);
+C(1:3,1:3)=eye(3);
+C(4:6,7:9)=eye(3);
+G = eye(12);
+Qw = 0.01 * eye(12);
+
+Qw = Qw + 1e-9 * eye(size(Qw));
+
+Rv = 0.01 * eye(12);
+
+L = lqr(p.A_lin', C', eye(12), eye(6))';
+
+eig(p.A_lin-L*C) 
